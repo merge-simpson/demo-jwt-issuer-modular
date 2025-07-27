5ac394f62a9d444a6d00e1794d2688c2a34c4a86
merge-simpson <letsdev@kakao.com>
2025-07-27 13:47:39 +0900

    build(jwt): add `jjwt-jackson` and `jackson-databind` runtime dependencies in `jwt-issuer` module
    enable jackson-based serializer implementation for `jwt-issuer` module

--- core/jwt/jwt-issuer/build.gradle.kts
+++ core/jwt/jwt-issuer/build.gradle.kts
@@ -2,5 +2,9 @@ dependencies {
     api(project(":jwt-api"))
 
     // jwt
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    api("io.jsonwebtoken:jjwt-impl:0.12.6")
+
+    // jackson
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+    runtimeOnly("com.fasterxml.jackson.core:jackson-databind:2.19.2")
 }
